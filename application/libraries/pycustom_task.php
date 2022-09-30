1949a54075b9c7fb022a8a6ccfc928b3d0bf6ce4
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2022-09-30 15:28:15 -0400

    Add a custom Python runner (WIP)

---
+++ application/libraries/pycustom_task.php
@@ -0,0 +1,47 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/* ==============================================================
+ *
+ * Custom Python
+ *
+ * ==============================================================
+ *
+ * @copyright  2014 Richard Lobb, University of Canterbury
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once('application/libraries/LanguageTask.php');
+
+
+class Pycustom_Task extends Task {
+    public function __construct($filename, $input, $params) {
+        parent::__construct($filename, $input, $params);
+        $this->default_params['interpreterargs'] = array('-BESs');
+	$this->default_params
+    }
+
+    public static function getVersionCommand() {
+        return array('python --version', '/Python ([0-9._]*)/');
+    }
+
+    // A default name for Python2 programs
+    public function defaultFileName($sourcecode) {
+        return 'main.py';
+    }
+
+    public function compile() {
+        $this->executableFileName = $this->sourceFileName;
+    }
+
+
+    public function getExecutablePath() {
+        // depend on PATH being set appropriately in the environment.
+        return 'python';
+     }
+
+
+     public function getTargetFile() {
+         return $this->sourceFileName;
+     }
+};
+
